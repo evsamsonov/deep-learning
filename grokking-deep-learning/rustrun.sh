d08f4806ae5c868e65b9bf08685f6ca87e36f16e
Evgeny Samsonov <bitniks@yandex.ru>
2021-05-07 23:20:17 +0300

    Check rustc error

--- grokking-deep-learning/rustrun.sh
+++ grokking-deep-learning/rustrun.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-rustc "$1"
-# todo проверить ошибку
+if ! rustc --edition 2018 "$1"; then
+    echo "Failed to compile"
+    return
+fi
 
 exec_file=$(echo "$1" | cut -f 1 -d '.')
 ./"$exec_file"
