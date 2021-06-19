8d53dd35338b6d28d3d5ce8e38c753106218c3a6
Evgeny Samsonov <bitniks@yandex.ru>
2021-06-19 14:21:41 +0300

    Hot cold method

---
+++ grokking-deep-learning/4_cold_hot_method.py
@@ -0,0 +1,33 @@
+def neural_network( input_value: float, weight: float) -> float:
+    return weight * input_value
+
+
+def get_error(prediction: float, real: float) -> float:
+    return (prediction - real) ** 2
+
+
+def main():
+    weight = 0.5
+    input_value = 0.5
+    real = 0.8
+
+    step = 0.001
+    for i in range(1101):
+        prediction = neural_network(input_value, weight)
+        mead_squared_error = get_error(prediction, real)
+        print("Error: " + str(mead_squared_error) + " Prediction: " + str(prediction))
+
+        up_prediction = neural_network(input_value, weight + step)
+        up_error = get_error(up_prediction, real)
+
+        down_prediction = neural_network(input_value, weight - step)
+        down_error = get_error(down_prediction, real)
+
+        if down_error < up_error:
+            weight -= step
+        else:
+            weight += step
+
+
+if __name__ == '__main__':
+    main()
